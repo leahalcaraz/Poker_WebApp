462d4567c22269f57a5558126b5d5afbaa159566
leahalcaraz <60593874+leahalcaraz@users.noreply.github.com>
2020-02-06 13:14:18 +0800

    Add files via upload
    Build.1.0.0.3

---
+++ Poker_WebApp/Poker_WebApp/PokerProgram.cs
@@ -0,0 +1,23 @@
+﻿using Poker_WebApp.Model;
+using System;
+using System.Configuration;
+
+namespace Poker_WebApp
+{
+    public class PokerProgram
+    {
+        #region Default
+        private static readonly string defaultNumberOfPlayers = ConfigurationManager.AppSettings["NumberOfPlayers"].ToString();
+        #endregion
+        public static void Main()
+        {
+            var poker = new PokerGame(int.Parse(defaultNumberOfPlayers));
+            poker.InitializeDeck();
+            poker.InitializeGame();
+            Console.WriteLine(poker.Hands());
+            Console.WriteLine(poker.Winner());
+            Console.ReadLine();
+        }
+    }
+}
+
